25369f41aeccb59f12520e6556c1508ec45dbc08
Tukhfatullin Zakir <zakirtuckhfatullin@gmail.com>
2025-05-16 14:05:24 +0600

    templates

---
+++ TemplatesAndException_code/task_1.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+template <typename T>
+T avg(T* arr, int size) {
+    T sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum / size;
+}
+
+template <typename T>
+void get(T* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 100;
+    }
+}
+
+template <>
+void get<char>(char* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = 'A' + rand() % 26;
+    }
+}
+
+template <typename T>
+void print(T* arr, int size, char a) {
+    for (int i = 0; i < size; i++) {
+        cout << a << "[" << i << "]=" << arr[i] << "\t";
+    }
+    cout << endl;
+}
+
+int main() {
+    srand(time(0));
+    setlocale(LC_ALL, "rus");
+
+    int intn, doublen, longn, charn;
+    cout << "Введите количество переменных для массива int: "; cin >> intn;
+    int* arrint = new int[intn];
+    get(arrint, intn);
+
+    cout << "Введите количество переменных для массива double: "; cin >> doublen;
+    double* arrdouble = new double[doublen];
+    get(arrdouble, doublen);
+
+    cout << "Введите количество переменных для массива long: "; cin >> longn;
+    long* arrlong = new long[longn];
+    get(arrlong, longn);
+
+    cout << "Введите количество переменных для массива char: "; cin >> charn;
+    char* arrchar = new char[charn];
+    get(arrchar, charn);
+
+    cout << "Массив int I:" << endl; print(arrint, intn, 'I'); cout << "Среднее: " << avg(arrint, intn) << endl;
+    cout << "Массив double D:" << endl; print(arrdouble, doublen, 'D'); cout << "Среднее: " << avg(arrdouble, doublen) << endl;
+    cout << "Массив long L:" << endl; print(arrlong, longn, 'L'); cout << "Среднее: " << avg(arrlong, longn) << endl;
+    cout << "Массив char C:" << endl; print(arrchar, charn, 'C'); cout << "Среднее: " << static_cast<int>(avg(arrchar, charn)) << endl;
+
+    delete[] arrint;
+    delete[] arrdouble;
+    delete[] arrlong;
+    delete[] arrchar;
+
+    return 0;
+}
---
+++ TemplatesAndException_code/task_2.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+template <typename mlk>
+struct node {
+    mlk value;
+    node* next;
+};
+template <typename mlk>
+node<mlk>* postroenie(node<mlk>*& ko) {
+    cout << "Вводите элементы очереди (введите 0 для завершения): " << endl;
+    mlk val;
+    node<mlk>* no = nullptr;
+    cin >> val;
+    while (val != 0) {
+        node<mlk>* t = new node<mlk>;
+        t->value = val;
+        t->next = nullptr;
+        if (no == 0) {
+            no = ko = t;
+        }
+        else {
+            ko->next = t;
+            ko = t;
+        }
+        cin >> val;
+    }
+    cout << "Очередь построена" << endl;
+    return no;
+}
+template <typename mlk>
+void print(node<mlk>* no) {
+    node<mlk>* r = no;
+    cout << "Начало очереди" << endl;
+    while (r != 0) {
+        cout << "\t" << r->value << "\n";
+        r = r->next;
+    }
+    cout << "Конец очереди!" << endl;
+}
+int main() {
+    setlocale(LC_ALL, "rus");
+    node<int>* into = nullptr;
+    node<double>* doubleo = nullptr;
+    node<long>* longo = nullptr;
+
+    cout << "Создание очереди int: " << endl;
+    into = postroenie(into);
+    print(into);
+
+    cout << "Создание очереди double:" << endl;
+    doubleo = postroenie(doubleo);
+    print(doubleo);
+
+    cout << "Создание очереди char:" << endl;
+    longo = postroenie(longo);
+    print(longo);
+
+    return 0;
+}
